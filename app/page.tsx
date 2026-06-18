fd122656c63403825915ec73d04d680bebc2be4c
siidigitalcreative-engine <siidigitalcreative@gmail.com>
2026-06-18 08:21:15 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import EMDCApp from "@/components/EMDCApp";
+"use client";
+import dynamic from "next/dynamic";
+
+const EMDCApp = dynamic(() => import("@/components/EMDCApp"), { ssr: false });
 
 export default function Page() {
   return <EMDCApp />;
